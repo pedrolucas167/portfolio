a824c41f75ccaf8e4a37d7afca0c5464f9873358
pedrolucas167 <poloniosh@outlook.com>
2025-11-27 22:25:33 -0300

    chore: add gh-pages as a dependency in package.json and package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   root: '.',
-  base: './',
+  base: '/portfolio/',
   publicDir: 'public',
   plugins: [react(), tailwindcss()],
   build: {
